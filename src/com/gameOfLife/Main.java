b9f31976235f085c70193e6a912a785d5f4bb34a
Adam Musil <musil28@student.vspj.cz>
2020-09-22 10:53:08 +0200

    upraven main pro MVC

--- src/com/gameOfLife/Main.java
+++ src/com/gameOfLife/Main.java
@@ -1,53 +1,14 @@
 package com.gameOfLife;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    int size = 50;
 
-        Scanner scan = new Scanner(System.in);
+    Board board = new Board(size);
+    GameOfLifeGUI window = new GameOfLifeGUI();
+    Controller controller = new Controller(board, window);
 
-        int fieldSize;
-        int evolutionCount;
-
-        System.out.print("field size: ");
-        //fieldSize = scan.nextInt();
-
-        fieldSize = 10;
-
-        System.out.print("number of evolutions: ");
-        //evolutionCount = scan.nextInt();
-
-        evolutionCount = 10;
-
-        Board board = new Board(fieldSize);
-        GameOfLifeGUI gameOfLifeGui = new GameOfLifeGUI();
-
-
-        int i = 0;
-        do{
-            ++i;
-            printField(board.getCurrentGeneration(), board.getFieldSize());
-            System.out.println("Cells Alive = " + board.getLiveCellCount());
-
-            board.generateNewGeneratio0n();
-
-            gameOfLifeGui.setAliveLabel(board.getLiveCellCount());
-            gameOfLifeGui.setGenerationLabel(i);
-
-            System.out.println();
-        }while(i < evolutionCount);
-
-    }
-
-    public static void printField(boolean[][] field, int fieldSize){
-        for(var row : field){
-            for(var element : row){
-                System.out.print( element ? "0" : "-" );
-            }
-            System.out.println();
-        }
+    controller.start();
     }
 }
